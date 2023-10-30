d01d3e639267b3632fcf167056647a52b58768d9
vyacheslav sergeev <swf32@vk.com>
2023-10-30 23:38:52 +0300

    upd 1

--- app.js
+++ app.js
@@ -1,104 +1,33 @@
-let tg = window.Telegram.WebApp;
+// Проверка наличия tg.ThemeParams
+var tg = window.Telegram ? window.Telegram.WebApp : null;
 
-tg.expand();
+function updateColors() {
+	var colorElements = document.querySelectorAll('.color-item');
 
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#2cab37';
+	colorElements.forEach(function(element) {
+		var colorType = element.getAttribute('data-color');
+		var colorValue;
 
-let item = "";
+		if (tg && tg.ThemeParams[colorType]) {
+			colorValue = tg.ThemeParams[colorType];
+		} else {
+			// Значения по умолчанию (замените на свои)
+			switch(colorType) {
+				case 'bg_color':
+					colorValue = '#FFFFFF'; // пример
+					break;
+				// Добавьте другие цвета
+			}
+		}
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+		element.querySelector('.color-circle').style.backgroundColor = colorValue;
+	});
+}
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
+// Обработчик события изменения темы
+if (tg) {
+	tg.onEvent('themeChanged', updateColors);
+}
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
-
-
-
-
-
-
-
-
+// Инициализация при загрузке страницы
+updateColors();
